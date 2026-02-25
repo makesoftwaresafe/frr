makesoftwaresafe/frr

commit f8dafef42b694de723df256765df37f8405ef48e

bgpd: avoid socket activation for VRF instances
Refactor `bgp_need_listening()` to prevent unnecessary reactivation of
BGP listening sockets for non default vrf instances that are already
active.

This function now checks whether a listener is already associated with
the BGP instance before attempting to open a new socket.

Also modify `bgp_get()` to skip socket setup for VRF instances, which
are now handled more appropriately elsewhere based on configuration
changes like `neighbor` or `listen-range`.

Signed-off-by: Francois Dumontet <francois.dumontet@6wind.com>

bgpd/bgp_vty.c
@@ -4718,6 +4718,24 @@ static struct peer_group *listen_range_exists(struct bgp *bgp,
 	return NULL;
 }
 
+static void bgp_need_listening(struct bgp *bgp, struct vty *vty)
+{
+	struct listnode *node;
+	struct bgp_listener *listener;
+
+	for (ALL_LIST_ELEMENTS_RO(bm->listen_sockets, node, listener)) {
+		if (listener->bgp == bgp)
+			break;
+	}
+	if (listener == NULL) {
+		struct vrf *vrf;
+
+		SET_FLAG(bgp->flags, BGP_FLAG_VRF_MAY_LISTEN);
+		vrf = bgp_vrf_lookup_by_instance_type(bgp);
+		bgp_handle_socket(bgp, vrf, VRF_UNKNOWN, true);
+	}
+}
+
 DEFUN (bgp_listen_range,
        bgp_listen_range_cmd,
        "bgp listen range <A.B.C.D/M|X:X::X:X/M> peer-group PGNAME",
@@ -4783,6 +4801,9 @@ DEFUN (bgp_listen_range,
 		vty_out(vty, "%% Configure the peer-group first\n");
 		return CMD_WARNING_CONFIG_FAILED;
 	}
+
+	/* if need start listening */
+	bgp_need_listening(bgp, vty);
 
 	ret = peer_group_listen_range_add(group, &range);
 	return bgp_vty_return(vty, ret);
@@ -4935,6 +4956,8 @@ static int peer_remote_as_vty(struct vty *vty, const char *peer_str,
 					"%% Create the peer-group or interface first\n");
 				return CMD_WARNING_CONFIG_FAILED;
 			}
+			/* if need start listening */
+			bgp_need_listening(bgp, vty);
 			return CMD_SUCCESS;
 		}
 	} else {
@@ -4943,6 +4966,9 @@ static int peer_remote_as_vty(struct vty *vty, const char *peer_str,
 				"%% Can not configure the local system as neighbor\n");
 			return CMD_WARNING_CONFIG_FAILED;
 		}
+
+		/* if need start listening */
+		bgp_need_listening(bgp, vty);
 		ret = peer_remote_as(bgp, &su, NULL, &as, as_type, as_str);
 	}
 

bgpd/bgpd.h
@@ -667,6 +667,7 @@ struct bgp {
 #define BGP_FLAG_L3VNI_SCHEDULE_FOR_INSTALL (1ULL << 41)
 #define BGP_FLAG_L3VNI_SCHEDULE_FOR_DELETE  (1ULL << 42)
 #define BGP_FLAG_LINK_LOCAL_CAPABILITY	    (1ULL << 43)
+#define BGP_FLAG_VRF_MAY_LISTEN		    (1ULL << 44)
 
 	/* BGP default address-families.
 	 * New peers inherit enabled afi/safis from bgp instance.

commit 9003b6de98fff78ab304d0be5bb1073afa208dc6

bgpd: fix missing starting listening port
The listening port also needs to be activated for the interface neighbor, as well
as for the normal neighbor.

Fixes: f8dafef42b694de723df256765df37f8405ef48e ("bgpd: avoid socket activation for VRF instances")

Signed-off-by: anlan_cs <anlan_cs@126.com>

bgpd/bgp_vty.c
@@ -5416,6 +5416,9 @@ static int peer_conf_interface_get(struct vty *vty, const char *conf_if,
 		ret = peer_group_bind(bgp, NULL, peer, group, &as);
 	}
 
+	/* if need start listening */
+	bgp_need_listening(bgp, vty);
+
 	return bgp_vty_return(vty, ret);
 }
 